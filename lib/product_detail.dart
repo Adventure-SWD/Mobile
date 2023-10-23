6bfa41dbc4373361fb6288b1984ffd237943fcee
Lê Tuấn Kiệt <tuankietgg29@gmail.com>
2023-10-24 01:52:17 +0700

    Update product details

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -5,7 +5,7 @@ class ProductDetailPage extends StatelessWidget {
 
   const ProductDetailPage({Key? key, required this.heroTag}) : super(key: key);
 
-  @override
+  /*@override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -29,15 +29,107 @@ class ProductDetailPage extends StatelessWidget {
           )
         ],
       ),
+    );*/
+
+  @override
+  Widget build(BuildContext context) {
+    final product = _images[heroTag];
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Colors.white,
+                child: Hero(
+                  tag: heroTag,
+                  child: Image.network(_images[heroTag]),
+                ),
+              ),
+            ),
+          ),
+
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _product[heroTag],
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      "Giá bán: ${_price[heroTag]}",
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      _description[heroTag],
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Thêm vào giỏ hàng"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Mua ngay"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              // Các phần khác như phần "Sản phẩm liên quan", phần "Câu hỏi thường gặp", v.v.
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
 
 final List<String> _images = [
-  'https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/273935/pexels-photo-273935.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/462024/pexels-photo-462024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+  'https://th.bing.com/th/id/R.602cb9cd48da72f879ac18ea1c945f6e?rik=YU956u%2b9jFHGVA&pid=ImgRaw&r=0',
+  'https://cdn.tgdd.vn/Files/2020/08/19/1281398/cach-lam-khoai-tay-chien-trong-10-phut-voi-noi-chien-khong-dau-khoai-gion-de-lau-cung-khong-mem-202008191207088100.jpg',
+  'https://banhmihanoi.net/wp-content/uploads/2020/05/banh-mi-pate-2.jpg',
+  'https://th.bing.com/th/id/R.77b7a8dc14339b022b9adf648b7b2ce6?rik=3jI6i6z7G3St9A&pid=ImgRaw&r=0',
+  'https://beverages2u.com/wp-content/uploads/2019/05/0001200010010_B-1024x1024.jpg',
+  'https://th.bing.com/th/id/OIP.7E2w-tUiWknbqZLS_awCMAHaE4?pid=ImgDet&rs=1'
+];
+
+final List<String> _product = [
+  'Cơm chiên',
+  'Khoai tây chiên',
+  'Bánh mỳ',
+  'Cocacola',
+  'Pepsi',
+  'Cà phê'
+];
+
+final List<String> _price = [
+  '30,000',
+  '15,000',
+  '25,000',
+  '10,000',
+  '10,000',
+  '15,000'
+];
+
+final List<String> _description = [
+  'Cơm gà chiên nóng hổi vừa thồi vừa ăn',
+  'Khoai tây chiên rắc muối giòn tan',
+  'Bánh mỳ với nhiều loại nhân khác nhau',
+  'Nước ngọt có ga Cocacola',
+  'Nước ngọt có ga Pepsi',
+  'Cà phê sữa đá'
 ];
